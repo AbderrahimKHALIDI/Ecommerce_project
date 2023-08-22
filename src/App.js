f808219098c9cb2b05d536540299cefe5ca9e5cc
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-08-22 03:34:36 +0200

    add form input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {  Route, Routes } from "react-router-dom";
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shope/shope.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-sign-up.component";
 
 function App() {
   return (
-  <div>
-    <Header/>
+    <div>
+      <Header />
       <Routes>
-        <Route  path="/" Component={HomePage} />
+        <Route path="/" Component={HomePage} />
         <Route path="/shop" Component={ShopPage} />
+        <Route path="/signin" Component={SignInAndSignUpPage} />
       </Routes>
-      </div>
+    </div>
   );
 }
 
