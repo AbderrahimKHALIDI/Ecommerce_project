f864e5a6c2b8ee9b41d078a7ade0b77dfc3369c1
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-10-06 03:37:32 +0200

    add error boundaries

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { HomePageContainer } from "./homepage.styles";
 import Directory from "../../components/directory/directory.component";
-const HomePage = () => {
-  throw Error;
-  return(
+const HomePage = () => (
   
   <HomePageContainer>
     <Directory/>
   </HomePageContainer>
-)};
+);
 
 export default HomePage;
